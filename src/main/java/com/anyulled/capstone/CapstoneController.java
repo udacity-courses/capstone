2eeea9a70207e19145f7a920458b8f2e029c3a5b
Anyul Rivas <anyul.rivas_oropeza@roche.com>
2020-06-13 18:44:06 +0200

    Modifying the controller to accept environment variables.

--- src/main/java/com/anyulled/capstone/CapstoneController.java
+++ src/main/java/com/anyulled/capstone/CapstoneController.java
@@ -1,13 +1,19 @@
 package com.anyulled.capstone;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.String.format;
 
 /**
  * Controller.
  */
 @RestController
 public class CapstoneController {
+
+    @Value("${CAPSTONE_ENVIRONMENT:testEnvironment}")
+    private String environment;
 
     /**
      * Say hello function.
@@ -16,6 +22,6 @@ public class CapstoneController {
      */
     @GetMapping("/")
     public String sayHello() {
-        return "Hello Udacity! my name is Anyul Rivas";
+        return format("Hello Udacity! my name is Anyul Rivas. %s", environment);
     }
 }
